4041acba3aabe5e0a0d920d4fa0bd2b50d862a3b
Eduardo Lima <edulimacosta223@gmail.com>
2022-06-22 09:42:22 -0300

    Integrado a API de advices

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import "./App.css";
-import { BsFillDice5Fill } from "react-icons/bs";
+import { BsAppIndicator, BsFillDice5Fill } from "react-icons/bs";
+import axios from "axios";
+import { useState } from "react";
+
 function App() {
+  const [adviceID, setAdviceID] = useState();
+  const [advice, setAdvice] = useState();
+  async function handleClick() {
+    try {
+      const response = await axios.get("https://api.adviceslip.com/advice");
+      setAdviceID("ADVICE #" + response.data.slip.id);
+      setAdvice(response.data.slip.advice);
+    } catch {
+      alert("[ERRO]");
+    }
+  }
+  handleClick();
   return (
     <div className="App">
       <div className="container-generator">
-        <span className="advice-title">ADVICE #11</span>
+        <span className="advice-title">{adviceID}</span>
         <div className="container-advice">
-          <span className="advice">
-            Avoid mixing Ginger Nuts With other biscuits, ther contaminate. Keep
-            separated.
-          </span>
+          <span className="advice">{advice}</span>
           <div className="barras">
             <div className="barra"></div>
             <div className="barra"></div>
           </div>
         </div>
         <div className="container-button">
-          <button className="button">
+          <button className="button" onClick={handleClick}>
             <BsFillDice5Fill size="40" />
           </button>
         </div>
